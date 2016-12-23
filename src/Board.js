7171513aac35b5b14c01b3152749d293f0b749d7
Hyeunjoon Kim <hyeunjoonkim@Hyeunjoons-MacBook-Pro-2.local>
2016-12-23 12:00:31 -0800

    manage knight position in state

--- src/Board.js
+++ src/Board.js
@@ -3,12 +3,32 @@ import Square from './Square';
 import Knight from './Knight';
 
 export default class Board extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      knightPosition: this.props.knightPosition
+    }
+
+    this.changeToRandomPos = this.changeToRandomPos.bind(this)
+  }
+
+  changeToRandomPos() {
+    const randomPos = [Math.floor(Math.random() * 8), Math.floor(Math.random() * 8)]
+
+    this.setState({ knightPosition: randomPos })
+  }
+
+  componentDidMount() {
+    setInterval(() => { this.changeToRandomPos() }, 200);
+  }
+
   renderSquare(i) {
     const x = i % 8;
     const y = Math.floor(i / 8);
     const black = (x + y) % 2 === 1;
 
-    const [knightX, knightY] = this.props.knightPosition;
+    const [knightX, knightY] = this.state.knightPosition;
     const piece = (x === knightX && y === knightY) ?
       <Knight /> :
       null;
